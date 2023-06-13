c14eb3f92640b373c02a318531fb39586f57cbf9
okshi <johnstarhafiz@gmail.com>
2023-06-13 23:52:38 +0600

    Complete midbar for shopping cart app

--- src/shared/mid-bar/mid-bar.jsx
+++ src/shared/mid-bar/mid-bar.jsx
@@ -1,7 +1,28 @@
+import "./mid-bar.css";
+import logo from "../../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPhone, faSearch } from "@fortawesome/free-solid-svg-icons";
 const MidBar = () => {
   return (
-    <div>
-      <h1>This is MidBar</h1>
+    <div className="midbar">
+      <div className="cart_logo">
+        <img className="logo" src={logo} alt="" />
+      </div>
+      <div className="search_area">
+        <div className="search_input">
+          <input type="text" placeholder="Search Product" className="search" />
+          <div className="area">
+            <FontAwesomeIcon className="faIcon" size="xs" icon={faSearch} />
+          </div>
+        </div>
+      </div>
+      <div className="cell_phone">
+        <FontAwesomeIcon icon={faPhone} />
+        <div className="call_number">
+          <p>Call us now</p>
+          <p>+1 01 444 55 99</p>
+        </div>
+      </div>
     </div>
   );
 };
