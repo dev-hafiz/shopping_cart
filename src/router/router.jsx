5136fa89873965946f1e1366fc1f828066b13999
okshi <johnstarhafiz@gmail.com>
2023-06-14 14:26:25 +0600

    Fixed some routing issue

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,13 +3,15 @@ import Main from "../Layout/Main";
 import NotFound from "../components/not-found/not-found";
 import Shop from "../components/shop/shop";
 import ReviewOrder from "../components/review-order/review-order";
+import Home from "../components/home/home";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
     children: [
-      { path: "/", element: <Shop /> },
+      { path: "/", element: <Home /> },
+      { path: "home", element: <Home /> },
       {
         path: "shop",
         loader: () => {
